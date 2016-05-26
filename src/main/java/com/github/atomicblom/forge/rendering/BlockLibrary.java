c75805a9912989094c4a0656b52c6de8acc5d26b
Steven Blom <atomicblom@binaryvibrance.net>
2016-05-26 22:20:23 +0930

    Generalize registering Blocks

--- src/main/java/com/github/atomicblom/forge/rendering/BlockLibrary.java
+++ src/main/java/com/github/atomicblom/forge/rendering/BlockLibrary.java
@@ -4,6 +4,7 @@ import com.github.atomicblom.forge.rendering.common.Blocks.AtomicPedestalBlock;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @SuppressWarnings("WeakerAccess")
@@ -13,10 +14,11 @@ public final class BlockLibrary {
     public static final Block atomicPedestal = null;
 
     static void registerBlocks() {
-        Block atomicPedestal = new AtomicPedestalBlock()
-                .setRegistryName(Resources.Blocks.AtomicPedestal)
-                .setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-        GameRegistry.register(atomicPedestal);
-        GameRegistry.register(new ItemBlock(atomicPedestal).setRegistryName(Resources.Blocks.AtomicPedestal).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
+        registerBlockAndItem(new AtomicPedestalBlock(), Resources.Blocks.AtomicPedestal, CreativeTabs.BUILDING_BLOCKS);
+    }
+
+    private static void registerBlockAndItem(Block block, ResourceLocation name, CreativeTabs tab) {
+        GameRegistry.register(block.setRegistryName(name).setCreativeTab(tab));
+        GameRegistry.register(new ItemBlock(block).setRegistryName(name).setCreativeTab(tab));
     }
 }
